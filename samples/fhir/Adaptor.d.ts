fd12314fc35c73472e4016bd073e8c72fb44a046
optimization team <i.sadou@oxolo.com>
2023-12-11 10:22:18 +0100

    Testing optional info in instruction

--- samples/fhir/Adaptor.d.ts
+++ samples/fhir/Adaptor.d.ts
@@ -1,13 +1,13 @@
 /**
-  * Creates a new patient instance and includes it in the state data.
+  * Creates a new patient instance and adds it to the state data.
   * Sends a POST request to the /Patient endpoint.
   * @example
-  * createPatient({name: 'John Doe', age: 30}, callback)
+  * createPatient({name: 'John Doe', age: 30, gender: 'male'}, callback)
   * @function
-  * @param {Object} patientData - The data of the patient to be created. Should include at least a name and age.
+  * @param {Object} patient - The patient object containing name, age, and gender.
   * @param {Function} callback - A callback which is invoked with the resulting state at the end of this operation. Allows users to customize the resulting state. State.data includes the response from the server.
   * @example <caption>Create a new patient</caption>
-  * createPatient({name: 'John Doe', age: 30})
+  * createPatient({name: 'John Doe', age: 30, gender: 'male'})
   * @returns {Function} A function that updates the state with the created patient instance.
   */
-  export function createPatient(patientData: Object, callback?: Function): Operation;
+  export function createPatient(patient: {name: string, age: number, gender: string}, callback?: Function): Operation;
--- samples/fhir/Adaptor.js
+++ samples/fhir/Adaptor.js
@@ -1,41 +1,51 @@
 import {
-    expandReferences,
+  expandReferences,
 } from '@openfn/language-common';
 import axios from 'axios';
 
 /**
- * Creates a new patient instance and includes it in the state data.
+ * Creates a new patient instance and adds it to the state data.
  * Sends a POST request to the /Patient endpoint.
  * @example
- * createPatient({name: 'John Doe', age: 30}, callback)
+ * createPatient({name: 'John Doe', age: 30, gender: 'male'}, callback)
  * @function
- * @param {Object} patientData - The data of the patient to be created. Should include at least a name and age.
+ * @param {Object} patient - The patient object containing name, age, and gender.
  * @param {Function} callback - A callback which is invoked with the resulting state at the end of this operation. Allows users to customize the resulting state. State.data includes the response from the server.
  * @example <caption>Create a new patient</caption>
- * createPatient({name: 'John Doe', age: 30})
+ * createPatient({name: 'John Doe', age: 30, gender: 'male'})
  * @returns {Function} A function that updates the state with the created patient instance.
  */
-export function createPatient(patientData, callback) {
-    return state => {
-        const url = `${state.configuration.apiUrl}/Patient`;
-        const data = expandReferences(patientData)(state);
-        return axios
-            .post(url, data)
-            .then(response => {
-                const { id } = response.data;
-                const nextState = {
-                    ...state,
-                    references: [...state.references, id],
-                    data: response.data,
-                };
-                if (callback) return callback(nextState);
-                return nextState;
-            })
-            .catch(error => {
-                if (state.configuration.onError) {
-                    return state.configuration.onError(error, state);
-                }
-                throw error;
-            });
+export function createPatient(patient, callback) {
+  return state => {
+    const url = state.configuration.apiUrl + '/Patient';
+    const data = {
+      ...patient,
     };
+
+    const config = {
+      params: {
+        identifier: 'system',
+      },
+    };
+
+    const resolvedData = expandReferences(data)(state);
+
+    return axios
+      .post(url, resolvedData, config)
+      .then(response => {
+        const createdPatient = response.data;
+        const nextState = {
+          ...state,
+          data: {
+            ...state.data,
+            patient: createdPatient,
+          },
+        };
+        if (callback) return callback(nextState);
+        return nextState;
+      })
+      .catch(error => {
+        return error;
+      });
+  };
 }
